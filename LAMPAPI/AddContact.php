30fdf72eff289a8ca922973bc82d91496ee963f5
Benjamin Belizaire <BenjaminBelizaire@gmail.com>
2024-01-23 17:04:47 -0500

    modified it, so we won't have duplicate cotacts

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,5 +1,4 @@
 <?php
-
 $inData = getRequestInfo();
 
 $firstName = $inData["firstName"];
@@ -13,12 +12,24 @@ $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
-    $stmt = $conn->prepare("INSERT into Contacts (UserId, First_Name, Last_Name, Phone, Email) VALUES (?, ?, ?, ?, ?)");
+    // Check if a contact with the same information already exists for the user
+    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE UserId = ? AND First_Name = ? AND Last_Name = ? AND Phone = ? AND Email = ?");
     $stmt->bind_param("sssss", $userId, $firstName, $lastName, $phone, $email);
     $stmt->execute();
-    $stmt->close();
-    $conn->close();
-    returnWithError("");
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // A contact with the same information already exists
+        returnWithError("Contact already exists for this user");
+    } else {
+        // Insert the new contact
+        $stmt = $conn->prepare("INSERT into Contacts (UserId, First_Name, Last_Name, Phone, Email) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $userId, $firstName, $lastName, $phone, $email);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
 }
 
 function getRequestInfo()
@@ -37,5 +48,4 @@ function returnWithError($err)
     $retValue = '{"error":"' . $err . '"}';
     sendResultInfoAsJson($retValue);
 }
-
 ?>
